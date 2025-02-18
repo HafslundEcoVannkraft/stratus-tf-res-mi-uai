8d99a0d42a3e233315f8f36f84d3055baa6af937
Irene Fougner <irene.fougner@live.no>
2025-02-18 10:25:25 +0100

    client id output

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-output "uai_id" {
+output "id" {
   description = "The ID of the User Assigned Identity"
   value       = azurerm_user_assigned_identity.uai.id
+}
+
+output "client_id" {
+  description = "The ID of the User Assigned Identity"
+  value       = azurerm_user_assigned_identity.uai.client_id
 }
